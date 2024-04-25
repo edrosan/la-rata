86f88b466685b75164c5f7c716154da9bfb121e4
Eduardo Rodriguez <edo.rod.san@gmail.com>
2024-04-24 21:56:19 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <h1>Pues un cafecito, no?</h1>
-     <img src='https://c.tenor.com/3Wb9efNi86EAAAAd/tenor.gif' />
-    </>
+    <div className='w-full h-screen flex flex-col gap-8 bg-gray-900'>
+     <h1 className='w-full text-gray-300 text-center mt-8 text-4xl'>Pues un cafecito, no?</h1>
+     <div className='w-auto h-auto '>
+
+     <img className='w-full h-full ' src='https://c.tenor.com/3Wb9efNi86EAAAAd/tenor.gif' />
+     </div>
+    </div>
   )
 }
 
