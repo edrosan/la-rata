7d4d2b89996a1358b2be8a8ad8e3ca4e74d72d12
Eduardo Rodriguez <edo.rod.san@gmail.com>
2024-04-24 21:45:29 -0600

    update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Pues un cafecito, no?</h1>
+     <img src='https://c.tenor.com/3Wb9efNi86EAAAAd/tenor.gif' />
+    </>
+  )
+}
+
+export default App
